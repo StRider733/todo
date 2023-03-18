580324073c4f8663cad3ca153e46e2725cd047cc
StRider733 <118477683+StRider733@users.noreply.github.com>
2023-03-18 20:58:20 +0300

    todo App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [inputValue, setInputValue] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (!inputValue) return;
+    setTodos([...todos, { id: Date.now(), name: inputValue, completed: false }]);
+    setInputValue('');
+  };
+
+  const handleDelete = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const handleEdit = (id, newName) => {
+    const newTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          name: newName,
+        };
+      }
+      return todo;
+    });
+    setTodos(newTodos);
+  };
+
+  const handleCheckboxChange = (id) => {
+    const newTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          completed: !todo.completed,
+        };
+      }
+      return todo;
+    });
+    setTodos(newTodos);
+  };
+
+  const renderTodoItem = (todo) => (
+    <li key={todo.id}>
+      <input
+        type="checkbox"
+        checked={todo.completed}
+        onChange={() => handleCheckboxChange(todo.id)}
+      />
+      <span className={todo.completed ? 'completed' : ''}>{todo.name}</span>
+      <button className="edit-btn" onClick={() => handleEdit(todo.id, prompt('New name:', todo.name))}>
+        Edit
+      </button>
+      <button className="delete-btn" onClick={() => handleDelete(todo.id)}>Delete</button>
+    </li>
+  );
+
+  const completedTodos = todos.filter((todo) => todo.completed);
+  const incompleteTodos = todos.filter((todo) => !todo.completed);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <h1 className="app-heading">StRider To-Do List</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={inputValue} onChange={handleInputChange} className="input-field" />
+        <button type="submit" className="add-btn">Add Task</button>
+      </form>
+      <ul className="todo-list">
+        {incompleteTodos.map((todo) => renderTodoItem(todo))}
+      </ul>
+      {completedTodos.length > 0 && (
+        <>
+          <h2 className="completed-heading">Completed</h2>
+          <ul className="completed-list">
+            {completedTodos.map((todo) => renderTodoItem(todo))}
+          </ul>
+        </>
+      )}
     </div>
   );
 }
